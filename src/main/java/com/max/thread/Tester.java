967ee2cda927cfc09d5d642eb63acdda34633fc9
Max <windtalker600@gmail.com>
2019-02-12 13:11:50 +0800

    20180212A

--- src/main/java/com/max/thread/Tester.java
+++ src/main/java/com/max/thread/Tester.java
@@ -5,11 +5,17 @@ public class Tester {
         for (int i=0; i <10; i++){
             System.out.println("main:" +i);
         }
+        //下面是個匿名類別
         Thread thread = new Thread(){
             @Override
             public void run() { //複寫Thread身上的run方法
                 for (int i=0; i <10; i++) {
                     System.out.println("Thread:" + i);
+                    try { //處理一下可能的interrupt exception
+                        sleep(50);//利用Thread方法中的sleep方法, 睡一下50msec
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }; //這是一個修改過的Thread類別,不是全新的類別
@@ -22,12 +28,38 @@ public class Tester {
             @Override
             public void run() {
                 for (int i = 0; i < 10; i++) {
-                    System.out.println("Runnable" + i);
+                    System.out.println("Runnable lambda" + i);
+                    //無法直接使用thread身上的sleep,用Thread.sleep()方式達成:
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }; //把runnable這個物件生出來
         Thread r = new Thread(runnable); //再將runnable交到Thread()的建構子當中
         r.start(); //在呼叫這個thread.start
+        //Runnable - Lambda 這就是Lambda的用法,用在常常寫匿名類別
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Runnable" + i);
+                //無法直接使用thread身上的sleep,用Thread.sleep()方式達成:
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start(); //反正一樣要.start()  常常使用匿名類別,可以有更好的寫法,底下註解是原本的匿名類別寫法
+/*        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Runnable lambda" + i);
+                }
+            }
+        }).start(); //反正一樣要.start()  常常使用匿名類別,可以有更好的寫法*/
         System.out.println("main end");  //可以看到結果是main thread已經結束,而Thread還沒跑完
     }
 }
@@ -41,7 +73,12 @@ class MyThread extends Thread {
     public void run() {  //複寫Thread身上的run方法
         for (int i = 0; i < 10; i++) { //快速寫法 10.fo 就可以寫出這個1到10
             System.out.println(getName() + ":" + i);
-
+            try {
+                sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
+//一但有加上sleep 50msec後,整個thread就會交叉進行
